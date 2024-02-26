405e0e5aaf3d338ba147e2d3cabc65933109e290
Admin Stagiaire <chayanne.pir@gmail.com>
2024-02-26 15:25:20 +0100

    Pagination

--- src/components/pages/Cartes.jsx
+++ src/components/pages/Cartes.jsx
@@ -1,17 +1,33 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ApiService from "../service/ApiService";
 
 function Cartes() {
-
     const api = new ApiService("http://localhost:8080/api/v1/cartes");
     const [cartes, setCartes] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(10);
 
     useEffect(() => {
-        api.get()
-            .then((response) => setCartes(response))
-            .catch((error) => alert(error.message))
-            .finally(() => console.log('GET terminé'))
+        const fetchData = async () => {
+            try {
+                const response = await api.get();
+                setCartes(response);
+            } catch (error) {
+                alert(error.message);
+            } finally {
+                console.log('GET terminé');
+            }
+        };
+
+        fetchData();
     }, []);
+
+    // Calculate the index range for the current page
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = cartes.slice(indexOfFirstItem, indexOfLastItem);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <>
@@ -29,7 +45,7 @@ function Cartes() {
                         </tr>
                     </thead>
                     <tbody>
-                        {cartes.map((carte) => (
+                        {currentItems.map((carte) => (
                             <tr key={carte.id}>
                                 <td>{carte.name}</td>
                                 <td>{carte.description}</td>
@@ -38,13 +54,26 @@ function Cartes() {
                                 <td>{carte.statForce}</td>
                                 <td>{carte.licenceId}</td>
                             </tr>
-                        )
-                        )}
+                        ))}
                     </tbody>
                 </table>
+
+                {/* Pagination buttons */}
+                <div className="pagination">
+                    {Array.from({ length: Math.ceil(cartes.length / itemsPerPage) }, (_, index) => (
+                        <button
+                        key={index + 1}
+                        onClick={() => paginate(index + 1)}
+                        className={currentPage === index + 1 ? 'active' : ''}
+                        >
+                        {index + 1}
+                        </button>
+                    ))}
+                </div>
+
             </div>
         </>
-    )
+    );
 }
 
-export default Cartes
+export default Cartes;
